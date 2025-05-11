7bdf87bca683a4df33fd88be176dff589b72cb31
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-05-11 23:02:25 +0700

    Google Button Jetpack Compose

--- app/src/main/java/com/learn/belajarjetpackcompose/GoogleButtonActivity.kt
+++ app/src/main/java/com/learn/belajarjetpackcompose/GoogleButtonActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.learn.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 
 class GoogleButtonActivity : ComponentActivity() {
@@ -19,29 +17,18 @@ class GoogleButtonActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BelajarJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                    )
+                {
+                    GoogleButton(
+                        text = "Sign Up with Google",
+                        loadingText = "Creating Account..."
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    BelajarJetpackComposeTheme {
-        Greeting2("Android")
     }
 }
