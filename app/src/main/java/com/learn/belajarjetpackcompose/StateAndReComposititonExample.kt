ba5d0636c37b628efbd096df528eda1b4da578f4
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-06-30 22:35:01 +0700

    Belajar State dan Recomposition Jetpack Compose

---
+++ app/src/main/java/com/learn/belajarjetpackcompose/StateAndReComposititonExample.kt
@@ -0,0 +1,65 @@
+package com.learn.belajarjetpackcompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.learn.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
+
+class StateAndReComposititonExample : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BelajarJetpackComposeTheme {
+                StateAndReComposititon()
+            }
+        }
+    }
+}
+
+@Composable
+fun StateAndReComposititon() {
+    var count = remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                count.value++
+            }
+        ) {
+            Text(text = "Count = " + count.value)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StateAndReCompositionPreview() {
+    StateAndReComposititon()
+}
